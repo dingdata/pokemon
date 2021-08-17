84f449736c1e18919cb68d5aa926e244cf59c001
dingdata <dingbro@gmail.com>
2021-08-17 10:17:15 +0800

    Production

--- src/components/PokemonGallery.js
+++ src/components/PokemonGallery.js
@@ -2,13 +2,24 @@ import React from "react";
 import POKEMON from "../pokemon/pokemon";
 import PokemonCard from "./PokemonCard";
 
+const style = {
+  header: {
+    fontSize: "100px",
+    color: "red",
+    textAlign: "center",
+  },
+};
+
 function PokemonGallery() {
   //console.log(POKEMON);
   return (
-    <div className="pokemonGallery">
-      {POKEMON.map((pokemon) => (
-        <PokemonCard pokemon={pokemon}></PokemonCard>
-      ))}
+    <div>
+      <div style={style.header}>PokeDex</div>
+      <div className="pokemonGallery">
+        {POKEMON.map((pokemon) => (
+          <PokemonCard pokemon={pokemon}></PokemonCard>
+        ))}
+      </div>
     </div>
   );
 }
